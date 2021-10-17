48642bb845869004821e38fcb5a968a61e5d5811
Margus Kaidja <margusk@gmail.com>
2021-10-17 18:36:39 +0000

    Add copyright information to each script.

--- src/Exception/OpenSSLCallFailedException.php
+++ src/Exception/OpenSSLCallFailedException.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the openssl-wrapper package.
+ *
+ * @author  Margus Kaidja <margusk@gmail.com>
+ * @link    https://github.com/marguskaidja/php-openssl-wrapper
+ * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ */
+
 declare(strict_types=1);
 
 namespace margusk\OpenSSL\Wrapper\Exception;
